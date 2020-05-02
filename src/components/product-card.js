9c208bef1938f8b28368f15faf7dd55653fe2d0b
Jeremy Milledge <jez.milledge@gmail.com>
2020-05-02 13:56:11 +1000

    setup tailwind, create component

---
+++ src/components/product-card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <h2>Hi</h2>
+      <p>this is a sentence</p>
+    </div>
+  );
+};
+
+export default ProductCard;
