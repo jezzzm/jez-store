af77bdac43546ba12bbf1ad180efb0a2ef9787c6
Jeremy Milledge <jez.milledge@gmail.com>
2020-05-09 18:25:36 +1000

    update tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
+import generateDemoProducts from '../fixtures/generate-demo-products';
 
 test('<App />', () => {
-  const { getByLabelText, getByRole } = render(<App />);
+  const products = generateDemoProducts(1);
+  const { getByLabelText, getByRole } = render(<App products={products} />);
   expect(getByRole('main')).toBeInTheDocument();
   expect(getByLabelText('Product List')).toBeInTheDocument();
 });
